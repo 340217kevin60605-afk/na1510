74a8c2ab7494383ba98f16e7606918756637caa8
柯于菱 <340217kevin60605@gmail.com>
2026-06-19 18:47:26 +0800

    修復CSS排版和圓角不見的問題

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'   // 👈 這行超重要！就是它負責載入所有漂亮排版和圓角
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
