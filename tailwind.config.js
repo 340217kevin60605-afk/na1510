8064eedb62ad66dc4c0b67cc778287b3f136661e
柯于菱 <340217kevin60605@gmail.com>
2026-06-19 18:18:58 +0800

    修正畫面樣式問題

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-export default { content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], theme: { extend: {} }, plugins: [] }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 這一行非常重要，告訴 Tailwind 去哪裡找 CSS class
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
